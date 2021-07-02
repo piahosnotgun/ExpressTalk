04ff9f7401e1871fbdcf754752151ee593e6d7af
Del-Los-Leones <dellosleones@pusan.ac.kr>
2021-07-02 22:59:52 +0000

    express session 사용하도록 설정

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 let Router = require('./router/router.js');
 let express = require('express');
 let app = express();
+var session = require('express-session');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('html', require('ejs').renderFile);
+
 app.use(express.static('assets'));
+app.use(session({
+ secret: 's3ss1ons3cr3T#$',
+ resave: false,
+ saveUninitialized: true
+}));
 
 let router = new Router(app);
 router.init();
--- router/router.js
+++ router/router.js
@@ -5,7 +5,12 @@ class Router {
 		this.init();
 	}
 	init(){
-		this.app.get('/', (req, res)=>res.render('index.ejs'));
+		this.app.get('/', (req, res)=>{
+			let session = req.session;
+			if(! session.user){
+				res.render('login');
+			}
+		});
 	}
 }
 module.exports = Router;
