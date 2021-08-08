c5741db784fd1aa8b40efbde44295804eecd1dd0
Del-Los-Leones <dellosleones@pusan.ac.kr>
2021-08-08 13:50:48 +0000

    진짜 미친 짓을 한거였네.. 한동안 안 건들고 갖다버렸다가 드디어 오류 고침

--- src/user.js
+++ src/user.js
@@ -21,12 +21,17 @@ class User {
 			}
 		});
 		this.tc.on('chat', (data, channel) => {
+			try {
+				
 			let sender = data.getSenderInfo(channel);
 			let name = sender.nickname;
 			let msg = data.text ?? '(지원되지 않는 메시지 형태입니다)';
 			// this.sendChannelList(this.socket, this.tc);
 			this.channelList = this.getChannelList(this.tc);
-			this.onMessage(this.socket, this.tc, sender, msg, channel.id.toString());
+			this.onMessage(this.socket, this.tc, sender, msg, channel.channelId.toString());
+			} catch(e){
+				console.log(e);
+			}
 		});
 	}
 	onMessage(client, tc, sender, msg, channelId) {
