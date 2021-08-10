206d008542a61f5f4612871c8ba8358d58e7f215
Del-Los-Leones <dellosleones@pusan.ac.kr>
2021-08-10 12:17:25 +0000

    와!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 let socket = io();
 let channelList = {};
 let currentChannel = ''; //id
+
+let chatroom = null;
+
 socket.on('data', (data) => {
 	if (data.type === 'channellist') {
 		channelList = data.content;
@@ -12,11 +15,14 @@ socket.on('message', (data) => {
 	let msg = data.text;
 	let sender = data.sender;
 	let channelId = data.channelId;
-	console.log(channelId + ' ' + sender + ' ' + msg);
+	if(channelId === currentChannel){
+		handleReceivedMessage(sender, msg);
+	}
 });
 window.onload = () => {
 	let msgElement = document.getElementById('chatinput');
 	let btn = document.getElementById('sendchat');
+	chatroom = document.getElementById('chatroomdisplay');
 	btn.onclick = () => {
 		let msg = msgElement.value;
 		if(msg ==='')
@@ -25,6 +31,38 @@ window.onload = () => {
 		sendMessage(msg, currentChannel);
 	};
 };
+function handleSelfMessage(msg){
+	let container = document.createElement('div');
+	container.classList.add('self');
+	let sender = document.createElement('p');
+	sender.appendChild(document.createTextNode('You'));
+	sender.classList.add('sender');
+	let balloon = document.createElement('div');
+	let message = document.createElement('p');
+	message.appendChild(document.createTextNode(msg));
+	balloon.classList.add('balloon');
+	message.classList.add('message');
+	balloon.appendChild(message);
+	container.appendChild(sender);
+	container.appendChild(balloon);
+	chatroom.appendChild(container);
+}
+function handleReceivedMessage(sender, msg){
+	let container = document.createElement('div');
+	container.classList.add('received');
+	let s = document.createElement('p');
+	s.appendChild(document.createTextNode(sender));
+	s.classList.add('sender');
+	let balloon = document.createElement('div');
+	let message = document.createElement('p');
+	message.appendChild(document.createTextNode(msg));
+	balloon.classList.add('balloon');
+	message.classList.add('message');
+	balloon.appendChild(message);
+	container.appendChild(s);
+	container.appendChild(balloon);
+	chatroom.appendChild(container);
+}
 function updateRoom() {
 	for (let channelId in channelList) {
 		let chatListDisplay = document.getElementById('chatlistdisplay');
@@ -42,6 +80,7 @@ function updateRoom() {
 }
 function sendMessage(msg, channelId) {
 	socket.emit('message', { text: msg, channelId: channelId });
+	handleSelfMessage(msg);
 }
 function requestChannelList() {
 	socket.emit('request', { type: 'channelList' });
